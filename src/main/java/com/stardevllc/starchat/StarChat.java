fd53cb4de8a84af694d2a1bdf2ad2147fc3952a7
Firestar311 <slaterandrew0398@gmail.com>
2024-02-20 12:48:38 -0600

    Updated BoostedYAML usages to use the new Config utility from StarCore

--- src/main/java/com/stardevllc/starchat/StarChat.java
+++ src/main/java/com/stardevllc/starchat/StarChat.java
@@ -4,14 +4,12 @@ import com.stardevllc.starchat.channels.ChatChannel;
 import com.stardevllc.starchat.channels.GlobalChannel;
 import com.stardevllc.starchat.channels.StaffChannel;
 import com.stardevllc.starchat.rooms.ChatRoom;
+import com.stardevllc.starcore.Config;
+import com.stardevllc.starcore.StarCore;
 import com.stardevllc.starlib.registry.StringRegistry;
 import com.stardevllc.starmclib.color.ColorUtils;
-import dev.dejvokep.boostedyaml.YamlDocument;
-import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
-import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
-import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
-import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import net.milkbowl.vault.chat.Chat;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -23,32 +21,24 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.logging.Level;
 
 public class StarChat extends JavaPlugin implements Listener {
     public static String consoleNameFormat; //How the console name appears
     public static UUID consoleUniqueId; //The uuid to use for the console
     public static Chat vaultChat; //Vault chat hook
-    private static YamlDocument yamlConfig; //Main config
+    private Config mainConfig;
     private ChatChannel globalChannel, staffChannel; //Default channels
     private Map<UUID, String> playerChatSelection = new HashMap<>(); //Current player focus
-    
+
     private StringRegistry<ChatChannel> channelRegistry = new StringRegistry<>(); //All channels
     private StringRegistry<ChatRoom> roomRegistry = new StringRegistry<>(); //All rooms
 
     @Override
     public void onEnable() {
-        try {
-            yamlConfig = YamlDocument.create(new File(getDataFolder(), "config.yml"), getResource("config.yml"), GeneralSettings.builder().setUseDefaults(true).build(), LoaderSettings.DEFAULT, DumperSettings.DEFAULT, UpdaterSettings.DEFAULT);
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Could not load config file", e);
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+        mainConfig = new Config(new File(getDataFolder(), "config.yml"));
 
         if (!setupChat()) {
             getLogger().severe("Could not setup Vault Chat.");
@@ -56,80 +46,56 @@ public class StarChat extends JavaPlugin implements Listener {
             return;
         }
         
-        if (!yamlConfig.contains("console-name-format")) {
-            yamlConfig.set("console-name-format", "&4Console");
-            yamlConfig.getBlock("console-name-format").addComment("The name that the console appears as in chat spaces");
-        }
-        
-        if (!yamlConfig.contains("console-uuid")) {
-            yamlConfig.set("console-uuid", UUID.randomUUID().toString());
-            yamlConfig.getBlock("console-uuid").addComment("The Unique ID (UUID) to use for the Console throughout the plugin.");
-        }
-        
-        try {
-            yamlConfig.save();
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Error while saving config.yml", e);
-        }
-        
-        StarChat.consoleNameFormat = yamlConfig.getString("console-name-format");
-        try {
-            StarChat.consoleUniqueId = UUID.fromString(yamlConfig.getString("console-uuid"));
-        } catch (Exception e) {
-            getLogger().severe("Error while parsing console uuid from config, regenerating...");
-            StarChat.consoleUniqueId = UUID.randomUUID();
-            yamlConfig.set("console-uuid", StarChat.consoleUniqueId.toString());
-            try {
-                yamlConfig.save();
-            } catch (IOException ex) {
-                getLogger().log(Level.SEVERE, "Error while saving config.yml", ex);
-            }
-        }
-        
+        mainConfig.addDefault("console-name-format", "&4Console", "The name that the console appears as in chat spaces.");
+        mainConfig.save();
+
+        StarChat.consoleNameFormat = mainConfig.getString("console-name-format");
+        StarChat.consoleUniqueId = ((StarCore) Bukkit.getPluginManager().getPlugin("StarCore")).getConsoleUnqiueId(); //Replace usages with this one
+
         globalChannel = new GlobalChannel(new File(getDataFolder() + File.separator + "channels" + File.separator + "defaults", "global.yml"));
         this.channelRegistry.register(globalChannel.getSimplifiedName(), globalChannel);
-        
+
         staffChannel = new StaffChannel(new File(getDataFolder() + File.separator + "channels" + File.separator + "defaults", "staff.yml"));
         this.channelRegistry.register(staffChannel.getSimplifiedName(), staffChannel);
-        
+
         getServer().getPluginManager().registerEvents(this, this);
     }
-    
+
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
         String message = e.getMessage();
-        
+
         ChatSpace chatSpace = getPlayerFocus(player);
         if (e.isCancelled()) {
             if (chatSpace.isAffectedByPunishments()) {
                 return; //Muted message should be handled by the punishments plugin.
             }
         }
-        
+
         e.setCancelled(true);
         chatSpace.sendMessage(player, message);
     }
-    
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("chat")) {
             if (!(sender instanceof Player player)) {
                 sender.sendMessage(ColorUtils.color("&cOnly players can use that command."));
                 return true;
             }
-            
+
             if (!(args.length > 0)) {
                 sender.sendMessage(ColorUtils.color("&cUsage: /" + label + " <channelName>"));
                 return true;
             }
-            
+
             String channelName = args[0].toLowerCase();
             ChatSpace chatSpace = this.channelRegistry.get(channelName);
             if (chatSpace == null) {
                 sender.sendMessage(ColorUtils.color("&cSorry, but &e" + channelName + " is not a registered chat space."));
                 return true;
             }
-            
+
             if (chatSpace instanceof ChatChannel chatChannel) {
                 String sendPermission = chatChannel.getSendPermission();
                 if (!sendPermission.isEmpty() && !player.hasPermission(sendPermission)) {
@@ -137,7 +103,7 @@ public class StarChat extends JavaPlugin implements Listener {
                     return true;
                 }
             }
-            
+
             this.setPlayerFocus(player, chatSpace);
             sender.sendMessage(ColorUtils.color("&aSet your chat focus to &b" + chatSpace.getName()) + ".");
         }
@@ -147,7 +113,7 @@ public class StarChat extends JavaPlugin implements Listener {
     public ChatChannel getGlobalChannel() {
         return globalChannel;
     }
-    
+
     public ChatSpace getPlayerFocus(Player player) {
         String playerSelection = playerChatSelection.get(player.getUniqueId());
         ChatSpace space = this.channelRegistry.get(playerSelection);
@@ -157,17 +123,13 @@ public class StarChat extends JavaPlugin implements Listener {
 
         return space;
     }
-    
+
     public void setPlayerFocus(Player player, ChatSpace chatSpace) {
         setPlayerFocus(player, chatSpace.getSimplifiedName());
     }
-    
+
     public void setPlayerFocus(Player player, String chatSpace) {
         this.playerChatSelection.put(player.getUniqueId(), chatSpace);
-    }
-
-    public static YamlDocument getYamlConfig() {
-        return yamlConfig;
     }
 
     private boolean setupChat() {
--- src/main/java/com/stardevllc/starchat/channels/ChatChannel.java
+++ src/main/java/com/stardevllc/starchat/channels/ChatChannel.java
@@ -2,77 +2,39 @@ package com.stardevllc.starchat.channels;
 
 import com.stardevllc.starchat.ChatSpace;
 import com.stardevllc.starchat.StarChat;
+import com.stardevllc.starcore.Config;
 import com.stardevllc.starmclib.color.ColorUtils;
-import dev.dejvokep.boostedyaml.YamlDocument;
-import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
-import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
-import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
-import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 public class ChatChannel extends ChatSpace {
-    protected transient YamlDocument config; //Config to store information as channels are mainly config/command controlled, transient modifier allows StarData to ignore this field without having to depend on StarData directly
-    
+    protected transient Config config; //Config to store information as channels are mainly config/command controlled, transient modifier allows StarData to ignore this field without having to depend on StarData directly
+
     protected String viewPermission = ""; //Permission needed by players in order to view messages in this channel
     protected String sendPermission = ""; //Permission needed by players in order to send messages in this channel
-    
+
     public ChatChannel(String name, File configFile) {
         super(name);
-        try {
-            config = YamlDocument.create(configFile, GeneralSettings.builder().setUseDefaults(true).build(), LoaderSettings.DEFAULT, DumperSettings.DEFAULT, UpdaterSettings.DEFAULT);
-        } catch (IOException e) {}
+        config = new Config(configFile);
     }
-    
-    protected void createDefaults() {
-        if (!config.contains("name")) {
-            config.set("name", this.name);
-            config.getBlock("name").addComment("It is recommended to not change this name in the file and instead use commands.");
-        }
-        
-        if (!config.contains("formats.sender")) {
-            config.set("formats.sender", senderFormat);
-            config.getBlock("formats.sender").addComment("The format used when a Player or the Console sends a message in this channel.");
-        }
-        
-        if (!config.contains("formats.system")) {
-            config.set("formats.system", systemFormat);
-            config.getBlock("formats.system").addComment("The format used when channel related messages are sent.");
-        }
-        
-        if (!config.contains("permissions.view")) {
-            config.set("permissions.view", viewPermission);
-            config.getBlock("permissions.view").addComment("The permission that is required to have in order for a player to see messages in this channel.");
-        }
-        
-        if (!config.contains("permissions.send")) {
-            config.set("permissions.send", sendPermission);
-            config.getBlock("permissions.send").addComment("The permission that is required to have in order for a player to send messages in this channel.");
-        }
-        
-        if (!config.contains("formats.playerdisplayname")) {
-            config.set("formats.playerdisplayname", playerDisplayNameFormat);
-            config.getBlock("formats.playerdisplayname").addComments(List.of("The format used for formatting a player's name for the {displayname} argument.", "You can have {prefix}, {suffix} and {name}"));
-        }
-        
-        if (!config.contains("settings.affectedbypunishments")) {
-            config.set("settings.affectedbypunishments", affectedByPunishments);
-            config.getBlock("settings.affectedbypunishments").addComment("This controls if this channel is affected by the AsyncPlayerChatEvent being cancelled by other plugins (Most likely punishment plugins)");
-        }
 
-        try {
-            config.save();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    protected void createDefaults() {
+        config.addDefault("name", this.name, "The name of the channel.", "It is recommended to not change this name in the file and instead use commands.");
+        config.addDefault("formats.sender", senderFormat, "The format used when a Player or the Console sends a chat message in this channel.");
+        config.addDefault("formats.system", systemFormat, "The format used when channel related messages are sent.");
+        config.addDefault("permissions.view", viewPermission, "The permission that is required to have in order for a player to receive messages from this channel.");
+        config.addDefault("permssions.send", sendPermission, "The permission that is required to have in order for a player to send messages in this channel.");
+        config.addDefault("formats.playerdisplayname", playerDisplayNameFormat, "The format used for formatting a player's name for the {displayname} variable.", "You can have {prefix}, {suffix} and {name}");
+        config.addDefault("settings.affectedbypunishments", affectedByPunishments);
+        config.addDefault("settings.affectedbypunishments", affectedByPunishments, "Control flag to allow messages to still be sent in this channel, even if the chat event is cancelled.");
+        config.save();
     }
-    
+
     public void loadSettings() {
         this.senderFormat = config.getString("formats.sender");
         this.systemFormat = config.getString("formats.system");
@@ -85,7 +47,7 @@ public class ChatChannel extends ChatSpace {
     @Override
     public void sendMessage(CommandSender sender, String message) {
         String formattedMessage = "";
-        
+
         if (sender == null) {
             formattedMessage = systemFormat;
         } else if (sender instanceof ConsoleCommandSender) {
@@ -95,10 +57,10 @@ public class ChatChannel extends ChatSpace {
                 player.sendMessage(ColorUtils.color("&cYou do not have permission to send messages in " + getName()));
                 return;
             }
-            
+
             formattedMessage = senderFormat.replace("{displayname}", formatPlayerDisplayName(player));
         }
-        
+
         formattedMessage = formattedMessage.replace("{message}", message);
         formattedMessage = ColorUtils.color(formattedMessage);
 
