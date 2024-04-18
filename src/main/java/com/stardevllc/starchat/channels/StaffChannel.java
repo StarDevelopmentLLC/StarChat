442f8f95474a6e19fcb79b413bd287a853976384
Firestar311 <slaterandrew0398@gmail.com>
2024-04-18 10:20:59 -0600

    Fixed a default format

--- src/main/java/com/stardevllc/starchat/channels/StaffChannel.java
+++ src/main/java/com/stardevllc/starchat/channels/StaffChannel.java
@@ -7,8 +7,8 @@ import java.io.File;
 public class StaffChannel extends ChatChannel {
     public StaffChannel(JavaPlugin plugin, File configFile) {
         super(plugin, "staff", configFile.toPath());
-        this.senderFormat.set("&2&l[&a&lSTAFF&2] {displayname}&8: &f{message}");
-        this.systemFormat.set("&2&l[&a&lSTAFF&2] &f{message}");
+        this.senderFormat.set("&2&l[&a&lSTAFF&2&l] {displayname}&8: &f{message}");
+        this.systemFormat.set("&2&l[&a&lSTAFF&2&l] &f{message}");
         this.viewPermission.set("starchat.channels.staff.view");
         this.sendPermission.set("starchat.channels.staff.send");
         this.config.save();
