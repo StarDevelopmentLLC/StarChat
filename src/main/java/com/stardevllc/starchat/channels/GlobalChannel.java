7360e60fc6afa3a39d1d38029d95ded2505c3741
Firestar311 <slaterandrew0398@gmail.com>
2024-05-11 16:23:23 -0500

    Changed the default global name

--- src/main/java/com/stardevllc/starchat/channels/GlobalChannel.java
+++ src/main/java/com/stardevllc/starchat/channels/GlobalChannel.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 public class GlobalChannel extends ChatChannel {
     public GlobalChannel(JavaPlugin plugin, File configFile) {
-        super(plugin, "Global", configFile.toPath());
+        super(plugin, "global", configFile.toPath());
         this.senderFormat.set("{displayname}&8: &r{message}");
         this.systemFormat.set("&r{message}");
         this.config.save();
