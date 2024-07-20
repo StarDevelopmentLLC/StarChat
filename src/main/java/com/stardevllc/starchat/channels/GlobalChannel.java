864be7226a3c026f9555d73531613fd1444529b6
Firestar311 <slaterandrew0398@gmail.com>
2024-07-19 20:36:14 -0500

    Added the ability to change the default name of the global channel

--- src/main/java/com/stardevllc/starchat/channels/GlobalChannel.java
+++ src/main/java/com/stardevllc/starchat/channels/GlobalChannel.java
@@ -5,8 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class GlobalChannel extends ChatChannel {
-    public GlobalChannel(JavaPlugin plugin, File configFile) {
-        super(plugin, "global", configFile.toPath());
+    public GlobalChannel(JavaPlugin plugin, String name, File configFile) {
+        super(plugin, name, configFile.toPath());
         this.senderFormat.set("{displayname}&8: &r{message}");
         this.systemFormat.set("&r{message}");
         this.config.save();
