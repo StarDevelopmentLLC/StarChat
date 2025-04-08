fd0bfe3740f38402a8a5cca56af36dd6f8eb1874
Firestar311 <slaterandrew0398@gmail.com>
2025-04-08 08:17:57 -0500

    Added recipients field and constructor overloads to ChatContext

--- src/main/java/com/stardevllc/starchat/context/ChatContext.java
+++ src/main/java/com/stardevllc/starchat/context/ChatContext.java
@@ -4,6 +4,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.*;
+
 public class ChatContext {
     public enum Source {
         CHAT_EVENT, DIRECT_SENDER, DIRECT_NO_SENDER
@@ -13,10 +15,16 @@ public class ChatContext {
     private CommandSender sender;
     private String message;
     private AsyncPlayerChatEvent chatEvent;
+    private final Set<UUID> recipients = new HashSet<>();
     
     public ChatContext(String message) {
         this.source = Source.DIRECT_NO_SENDER;
         this.message = message;
+    }
+    
+    public ChatContext(String message, Set<UUID> recipients) {
+        this(message);
+        this.recipients.addAll(recipients);
     }
     
     public ChatContext(CommandSender sender, String message) {
@@ -24,16 +32,30 @@ public class ChatContext {
         this.sender = sender;
         this.message = message;
     }
+    
+    public ChatContext(CommandSender sender, String messages, Set<UUID> recipients) {
+        this(sender, messages);
+        this.recipients.addAll(recipients);
+    }
 
     public ChatContext(AsyncPlayerChatEvent chatEvent) {
         this.source = Source.CHAT_EVENT;
         this.chatEvent = chatEvent;
     }
+    
+    public ChatContext(AsyncPlayerChatEvent chatEvent, Set<UUID> recipients) {
+        this(chatEvent);
+        this.recipients.addAll(recipients);
+    }
 
     public Source getSource() {
         return source;
     }
-
+    
+    public Set<UUID> getRecipients() {
+        return recipients;
+    }
+    
     public CommandSender getSender() {
         if (chatEvent != null) {
             return chatEvent.getPlayer();
@@ -46,6 +68,14 @@ public class ChatContext {
             return chatEvent.getMessage();
         }
         return message;
+    }
+    
+    public void setMessage(String message) {
+        if (chatEvent != null) {
+            chatEvent.setMessage(message);
+        } else {
+            this.message = message;
+        }
     }
 
     public AsyncPlayerChatEvent getChatEvent() {
